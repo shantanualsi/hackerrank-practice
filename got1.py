61b320d91a226259995003bab77f4d529e60324c
Shantanu Alshi <shantanuals@gmail.com>
2014-12-25 23:36:12 +0800

    Adding next 4 solutions

---
+++ got1.py
@@ -0,0 +1,13 @@
+string = raw_input()
+ 
+def has_palindrome(string):
+	for i in set(string):
+		sentence.count(i)
+
+found = False
+# Write the code to find the required palindrome and then assign the variable 'found' a value of True or False
+
+if not found:
+    print("NO")
+else:
+    print("YES")
