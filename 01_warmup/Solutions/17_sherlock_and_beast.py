4945d0aa08104515e205585936f493128bd371c3
Shantanu Alshi <shantanuals@gmail.com>
2015-01-02 16:46:21 +0800

    Correct solution to warmup 18

--- 01_warmup/Solutions/17_sherlock_and_beast.py
+++ 01_warmup/Solutions/17_sherlock_and_beast.py
@@ -1,8 +1,7 @@
-# This solution is incorrect and needs correction for some testcases
 
 def decent(n):
 	count=0;
-	if n == 3:
+	if n%3 == 0:
 		return count
 	while n > 0:
 		count = count +1
@@ -15,7 +14,8 @@ def decent(n):
 for _ in range(input()):
 	N = int(input());
 	X = decent(N);
+	print 'X = ', X*5, 'Y = ', (N-X*5)
 	if(X!=-1):
-		print '5' * (N-5*X) + '3' * 5*X;
+		print '5' * (N-X*5) + '3' * 5*X
 	else:
 		print X;
